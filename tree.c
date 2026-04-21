52bab6286cb0e4276f93d19ea202e8d33fdf6c01
Nidhi Yedida <nidhiyedida@gmail.com>
2026-04-21 18:04:35 +0000

    Phase 2: implement object write

--- tree.c
+++ tree.c
@@ -214,4 +214,12 @@ int tree_from_index(ObjectID *id_out) {
     size_t len;
 
     if (tree_serialize(&root, &data, &len) != 0) return -1;
+
+    if (object_write(OBJ_TREE, data, len, id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+    return 0;
 }
