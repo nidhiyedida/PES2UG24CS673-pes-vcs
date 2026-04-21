feb2d6040e3f750689020c399dd42da357af5659
Nidhi Yedida <nidhiyedida@gmail.com>
2026-04-21 19:36:34 +0000

    Phase 4: Read head

--- commit.c
+++ commit.c
@@ -194,8 +194,9 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    ObjectID tree_id;
+
+    if (tree_from_index(&tree_id) != 0) return -1;
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
 }
